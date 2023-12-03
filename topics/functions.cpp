b0cdae9832564cb8afb2dc8416776aa2a1efef00
TheBetterDark@TheBetterDark <91607604+TheBetterDark@users.noreply.github.com>
2023-12-03 12:53:45 +0000

    functions

--- topics/functions.cpp
+++ topics/functions.cpp
@@ -26,24 +26,50 @@ void happyBirthday(std::string name, int age)
 }
 
 // Function declaration only
-void helloWorld();
+double square(double length);
+double cube(double length);
+
+std::string concatStrings(std::string string1, std::string string2);
+
 // We are declaring the function here so that we can define it later.
 // This is important because the function must be declared before it is called as code is read from top to bottom.
 //
 
 int main()
 {
-    std::string name = "John";
+    std::string firstName = "John";
+    std::string lastName = "Doe";
     int age = 21;
 
-    happyBirthday(name, age); // Function call
-    helloWorld();             // Function call
+    happyBirthday(firstName, age); // Function call
+
+    std::string fullName = concatStrings(firstName, lastName); // Function call
+    std::cout << "Full name: " << fullName << '\n';
+
+    double length = 6.0;
+    double area = square(length); // Function call
+    double volume = cube(length); // Function call
+
+    std::cout << "Area: " << area << " cm^2" << '\n';
+    std::cout << "Volume: " << volume << " cm^3" << '\n';
 
     return 0;
 }
 
 // Function definition
-void helloWorld()
+double square(double length)
 {
-    std::cout << "Hello world!\n";
+    double result = length * length;
+    return result;
+}
+
+double cube(double length)
+{
+    double result = length * length * length;
+    return result;
+}
+
+std::string concatStrings(std::string string1, std::string string2)
+{
+    return string1 + " " + string2;
 }
